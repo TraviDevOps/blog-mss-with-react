4d1b3b433735ac09dc6c440ae756106d084455f9
Luis Lomba Martínez <luis.lomba@gruposantander.com>
2024-08-13 15:47:24 +0200

    Added emitting evens logic

--- comments/index.js
+++ comments/index.js
@@ -2,21 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 // this will act as our database in this example
-const commentsByPostId = {
-
-};
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
 
     const { content } = req.body;
@@ -25,6 +24,17 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content })
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post(`${process.env.MESSAGE_BROKER_URL}/events`, {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    }).catch((err) => {
+        console.log(err.message);
+    });
 
     res.status(201).send(comments);
 });
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -21,8 +22,16 @@ app.post('/posts', (req, res) => {
         id, title
     };
 
-    res.status(201).send(posts[id]);
+    await axios.post(`${process.env.MESSAGE_BROKER_URL}/events`, {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    }).catch((err) => {
+        console.log(err.message);
+    });
 
+    res.status(201).send(posts[id]);
 
 });
 
