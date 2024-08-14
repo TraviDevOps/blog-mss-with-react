39c93b50cb65ceb9e638bb248b1177c5edb59434
Luis Lomba Martínez <luis.lomba@gruposantander.com>
2024-08-14 14:36:24 +0200

    Added recovery logic to the query service

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +20,13 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/events', (req, res) => {
     const { type, data } = req.body;
 
+    handleEvent(type, data);
+
+    res.send({});
+});
+
+function handleEvent(type, data) {
+    console.log(`Handling event of type ${type}`);
     switch (type) {
         case 'PostCreated': {
             const { id, title } = data;
@@ -44,10 +52,14 @@ app.post('/events', (req, res) => {
         default:
             break;
     }
+}
 
-    res.send({});
-});
-
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002')
+
+    const events = await axios.get(`${process.env.MESSAGE_BROKER_URL}/events`);
+
+    for (let event of events.data) {
+        handleEvent(event.type, event.data);
+    }
 });
